31cbd0fe070777ab304e1c743ebd98fcedf1d227
gdouyang <gdouyang@isoftstone.com>
2017-11-01 18:01:50 +0800

    加入数据监控

---
+++ microservice-hystrix-dashboard/src/main/java/com/itmuch/cloud/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * 可视化的监控数据
+ *
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
