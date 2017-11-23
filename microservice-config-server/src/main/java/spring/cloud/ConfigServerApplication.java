c82a84e3e4333be7356c27282f6349c5a3505d6f
gdouyang <gdouyang@isoftstone.com>
2017-11-23 17:07:17 +0800

    加入统一配置

--- microservice-config-server/src/main/java/spring/cloud/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/spring/cloud/ConfigServerApplication.java
@@ -8,8 +8,19 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * 配置服务器<br>
  * http://localhost:8080/microservice-foo/dev
  * http://localhost:8080/microservice-foo-dev.properties
- * http://localhost:8080/microservice-foo-dev.yml
- *
+ * http://localhost:8080/microservice-foo-dev.yml <br>
+ * 
+ * 端点与配置文件映射规则：
+ * <ul>
+ * <li>/{application}/{profile}[/{label}]</li>
+ * <li>/{application}-{profile}.yml</li>
+ * <li>/{label}/{application}-{profile}.yml</li>
+ * <li>/{application}-{profile}.properties</li>
+ * <li>/{label}/{application}-{profile}.properties</li>
+ * </ul>
+ * 
+ * {application}微服务的名称(spring.appliction.name)； {label}对应Git仓库的分支，默认是mater；
+ * 
  */
 @EnableConfigServer
 @SpringBootApplication
--- microservice-simple-provider-user/src/main/java/spring/cloud/UserController.java
+++ microservice-simple-provider-user/src/main/java/spring/cloud/UserController.java
@@ -1,18 +1,32 @@
 package spring.cloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@GetMapping("/{id}")
-	public User findById(@PathVariable Long id){
+	public User findById(@PathVariable Long id) {
 		User findOne = this.userRepository.findOne(id);
 		return findOne;
+	}
+
+	@Value("${profile}")
+	private String profile;
+
+	@Value("${test_prop}")
+	private String aaa;
+
+	@GetMapping("/profile")
+	public String hello() {
+		return this.profile + "_" + aaa;
 	}
 }
